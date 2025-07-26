2150988454e6812efb8883384b19c400db139483
Antonio Jesús Ponce Vela <antoniojesuspv099@gmail.com>
2025-07-26 19:31:40 +0200

    chore: desactivando restricción Mayusculas en los mensajes de commit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,7 @@ import type { UserConfig } from "@commitlint/types";
 
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  "subject-case": [0], // Desactiva la restricción de mayúsculas/minúsculas
 };
 
 export default config;
